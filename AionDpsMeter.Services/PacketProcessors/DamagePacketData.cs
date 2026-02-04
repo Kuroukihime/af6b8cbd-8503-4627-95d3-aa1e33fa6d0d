f86024737f593f89d94f7a6235f1100960b5ad64
Egor Kyzmenko <Shiro2Nekoxd@gmail.com>
2026-02-04 18:17:32 +0300

    change pakcet reader logic and add damage block position correction for packets with missing varint

--- AionDpsMeter.Services/PacketProcessors/DamagePacketData.cs
+++ AionDpsMeter.Services/PacketProcessors/DamagePacketData.cs
@@ -6,14 +6,15 @@
         public int ActorId { get; init; }
         public int SkillCode { get; init; }
         public int DamageType { get; init; }
-        public long Damage { get; init; }
+        public long Damage { get; set; }
         public bool IsCritical { get; init; }
         public bool IsBackAttack { get; init; }
         public bool IsParry { get; init; }
         public bool IsPerfect { get; init; }
         public bool IsDoubleDamage { get; init; }
         public DateTime Timestamp { get; init; } = DateTime.Now;
-        public long[]? PotentialDamageValues { get; init; } 
+        public long[]? PotentialDamageValues { get; init; }
+        public int UnknownVarInt { get; set; }
 
         //for loggin
         public int SwitchValue { get; set; }
--- AionDpsMeter.Services/PacketProcessors/DamagePacketProcessor.cs
+++ AionDpsMeter.Services/PacketProcessors/DamagePacketProcessor.cs
@@ -29,58 +29,71 @@ namespace AionDpsMeter.Services.PacketProcessors
 
         public void Process04_38(byte[] packet)
         {
-            try
+            var (damageData, _, processResult) = ParseDamagePacket(packet);
+
+            if (damageData == null)
             {
-                var (damageData, _, processResult) = ParseDamagePacket(packet);
-
-                if (damageData == null)
-                {
-                    LogFailedPacketParsing(packet, processResult);
-                    return;
-                }
-
-                var playerDamage = CreatePlayerDamage(damageData);
-                if (playerDamage == null)
-                {
-                    logger.LogTrace($"NULL DAMAGE {BitConverter.ToString(packet)}");
-                    return;
-                }
-
-                LogAndProcessDamage(packet, damageData, playerDamage);
+                logger.LogTrace($"[04-38-FULL] PARSING FAILED {processResult} {BitConverter.ToString(packet)}");
+                return;
             }
-            catch (Exception ex) 
+
+            var playerDamage = CreatePlayerDamage(damageData);
+            if (playerDamage == null)
             {
-                logger.LogError(ex.Message, ex);
+                logger.LogTrace($"[04-38-FULL] PLAYER DAMAGE NULL {BitConverter.ToString(packet)}");
+                return;
             }
+
+            LogAndProcessDamage(packet, damageData, playerDamage, "04-38-FULL");
         }
 
         public void ProcessFF_FF(byte[] packet)
         {
-            try
+            var packetsResult = packetExtractor.ExtractPackets(packet);
+
+            if (packetsResult.Packets != null)
             {
-                var packetsResult = packetExtractor.ExtractPackets(packet);
-
-                if (packetsResult.Packets != null)
-                {
-                    ProcessExtractedPackets(packetsResult.Packets);
-                }
-
-                if (packetsResult.RemainingSegments != null)
-                {
-                   // ProcessRemainingSegments(packetsResult.RemainingSegments);
-                }
+                ProcessExtractedPackets(packetsResult.Packets);
             }
-            catch (Exception ex)
+
+            if (packetsResult.RemainingSegments != null)
             {
-                logger.LogError(ex.Message, ex);
+                ProcessRemainingSegments(packetsResult.RemainingSegments);
             }
         }
 
         private void ProcessExtractedPackets(List<byte[]> packets)
         {
             foreach (var dmgPacket in packets)
-            {
-                Process04_38(dmgPacket);
+            {   //Process04_38(dmgPacket);
+                var (damageData, _, processResult) = ParseDamagePacket(dmgPacket);
+
+                if (damageData == null)
+                {
+                    logger.LogDebug($"[04-38-EXTRACTED] PARSING FAILED {processResult} {BitConverter.ToString(dmgPacket)}");
+                    continue;
+                }
+
+                if (damageData.Damage <= 0) continue;
+
+                //sometimes varint value before damage is missing so damage takes its place.
+                //next byte after damage always number of sequences 1-4 so we can check if current damage value is actually came from this field
+                if (damageData.Damage <= 4)
+                {
+                    logger.LogError($"Corrected damage for skill {damageData.SkillCode} assuming damage block is 1 position left. Initial: {damageData.Damage} Corrected: {damageData.UnknownVarInt}");
+                    damageData.Damage = damageData.UnknownVarInt;
+                }
+
+
+
+                var playerDamage = CreatePlayerDamage(damageData);
+                if (playerDamage == null)
+                {
+                    logger.LogDebug($"[04-38-EXTRACTED] PLAYER DAMAGE NULL {BitConverter.ToString(dmgPacket)}");
+                    continue;
+                }
+
+                LogAndProcessDamage(dmgPacket, damageData, playerDamage, $"04-38-EXTRACTED");
             }
         }
 
@@ -102,35 +115,56 @@ namespace AionDpsMeter.Services.PacketProcessors
 
         private void ProcessEntityDamagePatterns(byte[] packet, Entity entity)
         {
-            int currentOffset = 0;
-
-            while (currentOffset < packet.Length)
+            try
             {
-                byte[] remainingPacket = packet.Skip(currentOffset).ToArray();
-                var dmgPatternIndex = FindValidDamagePatternEntityIndex(remainingPacket, entity);
+                int currentOffset = 0;
 
-                if (dmgPatternIndex < 2)
+                while (currentOffset < packet.Length)
                 {
-                    break;
-                }
+                    byte[] remainingPacket = packet.Skip(currentOffset).ToArray();
+                    var dmgPatternIndex = FindValidDamagePatternEntityIndex(remainingPacket, entity);
 
-                int absolutePatternIndex = currentOffset + dmgPatternIndex;
-                var (damageData, _, processResult) = ParseFromShortPacket(packet, absolutePatternIndex);
+                    if (dmgPatternIndex < 2)
+                    {
+                        break;
+                    }
 
-                if (damageData != null)
-                {
-                    ProcessAndLogDamageData(packet, absolutePatternIndex - 2, damageData);
-                }
-                else
-                {
-                    logger.LogTrace($"NULL DAMAGE {BitConverter.ToString(packet)}");
-                }
+                    int absolutePatternIndex = currentOffset + dmgPatternIndex;
+                    var (damageData, _, processResult) = ParseFromShortPacket(packet, absolutePatternIndex);
 
-                currentOffset = absolutePatternIndex + 4;
+
+                    if (damageData != null)
+                    {
+                        if (damageData.Damage > 0)
+                        {
+                            //sometimes varint value before damage is missing so damage takes its place.
+                            //next byte after damage always number of sequences 1-4 so we can check if current damage value is actually came from this field
+                            if (damageData.Damage <= 4)
+                            {
+                                logger.LogError(
+                                    $"Corrected damage for skill {damageData.SkillCode} assuming damage block is 1 position left. Initial: {damageData.Damage} Corrected: {damageData.UnknownVarInt}");
+                                damageData.Damage = damageData.UnknownVarInt;
+                            }
+
+                            ProcessAndLogDamageData(packet, absolutePatternIndex - 5, damageData, "FF-FF");
+                        }
+
+                    }
+                    else
+                    {
+                        logger.LogTrace($"[FF-FF] PLAYER DAMAGE NULL {BitConverter.ToString(packet)}");
+                    }
+
+                    currentOffset = absolutePatternIndex + 4;
+                }
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, $"Error processing damage patterns for entity {entity.Name} in packet {BitConverter.ToString(packet)}");
             }
         }
 
-        private void ProcessAndLogDamageData(byte[] packet, int offset, DamagePacketData damageData)
+        private void ProcessAndLogDamageData(byte[] packet, int offset, DamagePacketData damageData, string packType = "")
         {
             var playerDamage = CreatePlayerDamage(damageData);
             if (playerDamage == null)
@@ -139,12 +173,12 @@ namespace AionDpsMeter.Services.PacketProcessors
             }
 
             var pack = packet.Skip(offset).ToArray();
-            LogDamageDetails(pack, damageData, playerDamage);
+            LogDamageDetails(pack, damageData, playerDamage, packType);
             DamageReceived?.Invoke(this, playerDamage);
         }
 
         private (DamagePacketData? data, int bytesProcessed, PacketProcessResult result) ParseFromShortPacket(
-            byte[] packet, 
+            byte[] packet,
             int entityIdIndex)
         {
             try
@@ -157,9 +191,10 @@ namespace AionDpsMeter.Services.PacketProcessors
                 var reader = new DamagePacketReader(packet, entityIdIndex - 2);
 
                 if (!TryReadPacketDataWithTarget(reader, target.Id, out var packetData, out var processResult)) return (null, 0, processResult);
-                
+
+
                 if (packetData.ActorId == target.Id) return (null, reader.BytesProcessed, PacketProcessResult.ACTORID_EQUALS_TARGETID);
-                
+
                 return (packetData, reader.BytesProcessed, PacketProcessResult.SUCCES);
             }
             catch
@@ -231,7 +266,7 @@ namespace AionDpsMeter.Services.PacketProcessors
                 return false;
             }
 
-            if (!reader.SkipUnknownField())
+            if (!reader.ReadUnknownVarInt(out var unknownVarInt))
             {
                 processResult = PacketProcessResult.UNKNOWN_FIELD_ERROR;
                 return false;
@@ -257,7 +292,8 @@ namespace AionDpsMeter.Services.PacketProcessors
                 SwitchValue = switchValue,
                 FlagsOffset = flagsOffset,
                 DamageOffset = dmgOffset,
-                PotentialDamageValues = potentialDamageValues
+                PotentialDamageValues = potentialDamageValues,
+                UnknownVarInt = unknownVarInt
             };
 
             return true;
@@ -289,7 +325,8 @@ namespace AionDpsMeter.Services.PacketProcessors
                 SwitchValue = packetData.SwitchValue,
                 FlagsOffset = packetData.FlagsOffset,
                 DamageOffset = packetData.DamageOffset,
-                PotentialDamageValues = packetData.PotentialDamageValues
+                PotentialDamageValues = packetData.PotentialDamageValues,
+                UnknownVarInt = packetData.UnknownVarInt
             };
 
             return true;
@@ -389,34 +426,89 @@ namespace AionDpsMeter.Services.PacketProcessors
             };
         }
 
-        private void LogFailedPacketParsing(byte[] packet, PacketProcessResult processResult)
-        {
-            logger.LogTrace($"NULL DMG PACK {processResult} {BitConverter.ToString(packet)}");
-        }
 
-        private void LogAndProcessDamage(byte[] packet, DamagePacketData damageData, PlayerDamage playerDamage)
+        private void LogAndProcessDamage(byte[] packet, DamagePacketData damageData, PlayerDamage playerDamage, string packType = "")
         {
-            LogDamageDetails(packet, damageData, playerDamage);
+            LogDamageDetails(packet, damageData, playerDamage, packType);
             DamageReceived?.Invoke(this, playerDamage);
-            //if (playerDamage.Damage >= MinDamageThreshold)
-            //{
-            //    DamageReceived?.Invoke(this, playerDamage);
-            //}
+            ////if (playerDamage.Damage >= MinDamageThreshold)
+            ////{
+            ////    DamageReceived?.Invoke(this, playerDamage);
+            ////}
         }
 
-        private void LogDamageDetails(byte[] packet, DamagePacketData damageData, PlayerDamage playerDamage)
+        private void LogDamageDetails(byte[] packet, DamagePacketData damageData, PlayerDamage playerDamage, string packType = "")
         {
+            //if (playerDamage.Skill.ClassId != 13) return;
             string damageType = playerDamage.Damage < MinDamageThreshold ? "LOW DMG" : "VALID DMG";
 
-            logger.LogTrace($"{damageType}  {BitConverter.ToString(packet)}");
-            logger.LogTrace(
-                $"SKILL {playerDamage.Skill.Name} DMG {playerDamage.Damage} " +
-                $"SWVAL {damageData.SwitchValue} FLFGS_OFFSET {damageData.FlagsOffset} " +
-                $"DMG OFFSET {damageData.DamageOffset} CRIT {playerDamage.IsCritical} " +
-                $"PARRY {playerDamage.IsParry} BACK {playerDamage.IsBackAttack}, " +
-                $"DOUBLE {playerDamage.IsDoubleDamage}, PERFECT {playerDamage.IsPerfect}");
+
+
+
+            //logger.LogError($"{damageType}  {BitConverter.ToString(packet)}");
+            //logger.LogError(
+            //    $"[{packType}] SKILL {playerDamage.Skill.Name} DMG {playerDamage.Damage} " +
+            //    $"SWVAL {damageData.SwitchValue} FLFGS_OFFSET {damageData.FlagsOffset} " +
+            //    $"DMG OFFSET {damageData.DamageOffset}");
+            var reader = new DamagePacketReader(packet);
+            reader.ReadAndValidateHeader();
+            reader.ReadTargetId(out _);
+            reader.ReadAndValidateSwitchValue(out _);
+            reader.SkipFlagField();
+            reader.ReadActorId(out _);
+            reader.ReadSkillCode(out _);
+            reader.ReadDamageType(out _);
+            reader.ReadSpecialFlags(damageData.SwitchValue, out _, out _);
+            reader.ReadUnknownVarInt(out _);
+            reader.ReadAndValidateDamage(out _, out _, out _);
+
+
+            string Format(byte[]? bytes, int width)
+            {
+                if (bytes == null || bytes.Length == 0) return "null".PadRight(width);
+                var hex = BitConverter.ToString(bytes);
+                return hex.Length > width ? hex.Substring(0, width) : hex.PadRight(width);
+            }
+
+            string FormatByte(byte? value, int width)
+            {
+                if (value == null) return "null".PadRight(width);
+                var hex = value.Value.ToString("X2");
+                return hex.PadRight(width);
+            }
+
+            string headerBytes = Format(reader.HeaderBytes, 12);
+            string opcodeBytes = Format(reader.OpcodeBytes, 8);
+            string targetIdBytes = Format(reader.TargetIdBytes, 12);
+            string switchValueBytes = Format(reader.SwitchValueBytes, 8);
+            string flagFieldBytes = Format(reader.FlagFieldBytes, 12);
+            string actorIdBytes = Format(reader.ActorIdBytes, 12);
+            string skillCodeBytes = Format(reader.SkillCodeBytes, 20);
+            string damageTypeBytes = FormatByte(reader.DamageTypeByte, 3);
+            string specialFlagsBytes = FormatByte(reader.DamageFlagByte, 3);
+            string unknownSpecialByte = FormatByte(reader.UnknownSpecialByte, 3);
+            string unknownUnit32Value = Format(reader.UnknownUnit32Value, 12);
+            string unknownTailBytes = Format(reader.UnknownSpecialBlockTail, 12);
+            string unknownFieldBytes = Format(reader.UnknownVarintBytes, 12);
+            string damageBytes = Format(reader.DamageBytes, 12);
+            string leftoverBytes = Format(reader.LeftoverBytes, 30);
+
+            logger.LogError(
+                $"{damageType,-10} [{packType,-20}] " +
+                $"SKILL:{playerDamage.Skill.Name,-30} " +
+                $"DMG:{playerDamage.Damage,6} " +
+                $"SV:{switchValueBytes} " +
+                $"FF:{flagFieldBytes} " +
+                $"SC:{skillCodeBytes} " +
+                $"DT:{damageTypeBytes} " +
+                $"SF:{specialFlagsBytes} " +
+                $"00:{unknownSpecialByte} " +
+                $"UI:{unknownUnit32Value} " +
+                $"UT:{unknownTailBytes} " +
+                $"UF:{unknownFieldBytes} " +
+                $"D:{damageBytes} " +
+                $"L:{leftoverBytes}");
         }
     }
 
-   
 }
